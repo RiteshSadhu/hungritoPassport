3f0c5a291e96453bd862e60d55b3759aa4f9f4b1
Ritesh <riteshsadhu21@gmail.com>
2024-12-10 16:30:06 +0530

    Initial commit

---
+++ hungritoPassport/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for hungritoPassport project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hungritoPassport.settings')
+
+application = get_asgi_application()
