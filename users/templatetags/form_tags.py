ee58b94583f5cb22fc33202406e8ba8f592b35ba
Ritesh <riteshsadhu21@gmail.com>
2024-12-13 12:30:26 +0530

    filter added

--- users/templatetags/form_tags.py
+++ users/templatetags/form_tags.py
@@ -2,10 +2,28 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name='addclass')
 def addclass(field, css):
-    """
-    Add CSS classes to form fields
-    Usage: {{ form.field|addclass:"form-control" }}
-    """
     return field.as_widget(attrs={'class': css})
+
+
+@register.filter(name='addplaceholder')
+def addplaceholder(field, text):
+    return field.as_widget(attrs={'placeholder': text})
+
+
+@register.filter(name='addattrs')
+def addattrs(field, attrs):
+    """
+    Add multiple attributes to a field
+    Usage: {{ form.field|addattrs:"class:form-control,placeholder:Enter text" }}
+    """
+    attrs_dict = {}
+    attrs_list = attrs.split(',')
+
+    for attr in attrs_list:
+        key, value = attr.split(':')
+        attrs_dict[key.strip()] = value.strip()
+
+    return field.as_widget(attrs=attrs_dict)
